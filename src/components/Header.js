bc5922baf9ac26a54873350f696f24ee87b3314e
thanhduong0902 <thanhpot88@gmail.com>
2022-12-09 16:08:55 +0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,6 @@ const Header = () => {
   const logout = () => {
     setIsMenu(false);
     localStorage.clear();
-    
 
     dispatch({
       type: actionType.SET_USER,
@@ -107,7 +106,7 @@ const Header = () => {
                 initial={{ opacity: 0, scale: 0.6 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0.6 }}
-                className="w-48 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+                className=" w-52 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
               >
                 {/* phamthuy9a1pcx3@gmail.com */}
                 {user && user.email === "phamthuy9a1pcx3@gmail.com" && (
@@ -124,25 +123,28 @@ const Header = () => {
                     <MdAdd className="mr-2" />
                   </Link>
                 )}
-                <div
-                  onClick={() => setIsMenu(false)}
-                  className="flex flex-row items-center justify-between cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out"
-                >
-                  <p
-                    className="px-4 py-2 flex items-center gap-3   text-textColor text-base"
-                    onClick={logout}
+                {user && (
+                  <div
+                    onClick={() => setIsMenu(false)}
+                    className="flex flex-row items-center justify-between cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out"
                   >
-                    Đăng xuất
-                  </p>
-                  <MdLogout className="mr-2" />
-                </div>
+                    <p
+                      className="px-4 py-2 flex items-center gap-3   text-textColor text-base"
+                      onClick={logout}
+                    >
+                      Đăng xuất
+                    </p>
+                    <MdLogout className="mr-2" />
+                  </div>
+                )}
+
                 {!user && (
                   <Link to="/signin">
                     <div
                       onClick={() => setIsMenu(false)}
-                      className="flex flex-row items-center justify-between cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out"
+                      className="flex  flex-row items-center justify-between cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out"
                     >
-                      <p className="px-4 py-2 flex items-center gap-3   text-textColor text-base">
+                      <p className="px-4 py-2 flex items-center gap-3  text-textColor text-base">
                         Đăng nhập/ Đăng kí
                       </p>
                       <MdLogout className="mr-2" />
